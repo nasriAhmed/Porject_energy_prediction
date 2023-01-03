b12988e0228dd012985c06237cda00b0424a0db7
nasri ahmed <ahmednasri1991@gmail.com>
2023-01-03 11:32:11 +0100

    new commit

--- demo/demo.py
+++ demo/demo.py
@@ -8,5 +8,9 @@ data= pd.read_csv("./../data/data_energy.csv")
 
 #show 10 first lignes
 print(data.head(10))
+
+#show describe data
 print(data.describe())
+
+#show information data
 print(data.info())
